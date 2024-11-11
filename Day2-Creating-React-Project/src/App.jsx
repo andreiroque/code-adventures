7501743c70063bc5e6e62f90a7fb502700db604a
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-11 12:29:37 +0800

    Add functioning button and modify styling

--- Day2-Creating-React-Project/src/App.jsx
+++ Day2-Creating-React-Project/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="container">
       <h1>Hello World!</h1>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Click me! {counter}
+      </button>
     </div>
   );
 };
