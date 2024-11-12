1fcac272441c08d59cdae4914f0f68f6ceeec14d
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 13:30:15 +0800

    Improve program efficiency and add Error Handling

--- Day3-Java-Calendar-Project/Calendar.java
+++ Day3-Java-Calendar-Project/Calendar.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calendar{
@@ -8,18 +9,30 @@ public class Calendar{
     Scanner input = new Scanner(System.in);
 
     int yearInput;
-
-    System.out.print("Enter a year: ");
-    yearInput = input.nextInt();
-
-    for(int month = 1; month <= 12; month++){
-      LocalDate date = LocalDate.of(yearInput, month, 1);
-      System.out.println(date.getMonth());
-      for(int day = 1; day <= date.lengthOfMonth(); day++){
-        System.out.print(day + " ");
+    
+    try{
+      System.out.print("Enter a year: ");
+      yearInput = input.nextInt();
+  
+      for(int month = 1; month <= 12; month++){
+        LocalDate date = LocalDate.of(yearInput, month, 1);
+        System.out.println(date.getMonth());
+        for(int day = 1; day <= date.lengthOfMonth(); day++){
+          System.out.print(day + " ");
+        }
+        System.out.println();
+        System.out.println();
       }
-      System.out.println();
-      System.out.println();
+
+    }catch(InputMismatchException e){
+      input.nextLine();
+      System.out.println("\nError: " + e);
+      System.out.println("Please enter a YEAR ...\n");
+      //Loop back to main to restart the process.
+      main(args); //Remove the "args" if you get an error.
+
+    }finally{
+      input.close();
     }
 
   }
