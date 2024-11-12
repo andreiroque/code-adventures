cdd2197d43f0ae84826140ca32f2ad31a2ddc58a
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 11:24:36 +0800

    Add working navigation links

--- Day3-React-Small-Project-1/src/App.jsx
+++ Day3-React-Small-Project-1/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import NavList from "./NavList";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Hehe</h1>
-    </div>
+    <>
+      <NavList />
+      <div className="main">
+        <div className="container" id="home">
+          <h1>Home</h1>
+        </div>
+        <div className="container" id="about">
+          <h1>About</h1>
+        </div>
+        <div className="container" id="projects">
+          <h1>Projects</h1>
+        </div>
+        <div className="container" id="contacts">
+          <h1>Contacts</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ Day3-React-Small-Project-1/src/NavList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavList = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Project</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavList;
