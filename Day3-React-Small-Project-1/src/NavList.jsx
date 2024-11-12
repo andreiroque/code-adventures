250e06d79daffcf7d9305bc8bc5ddca5ce973250
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 11:37:37 +0800

    Add styling to navigation bar

--- Day3-React-Small-Project-1/src/NavList.jsx
+++ Day3-React-Small-Project-1/src/NavList.jsx
@@ -2,19 +2,27 @@ import React from "react";
 
 const NavList = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#home">Home</a>
+    <nav className="navbar bg-body-tertiary">
+      <ul className="navbar nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#home">
+            Home
+          </a>
         </li>
-        <li>
-          <a href="#about">About</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#about">
+            About
+          </a>
         </li>
-        <li>
-          <a href="#projects">Project</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#projects">
+            Project
+          </a>
         </li>
-        <li>
-          <a href="#contacts">Contacts</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#contacts">
+            Contacts
+          </a>
         </li>
       </ul>
     </nav>
