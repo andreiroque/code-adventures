3a2a3ece168a2b13f0cfeca95cabc5a225f24d02
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 21:23:04 +0800

    Add Error Handling

--- Day3-Scuffed-Fibonacci-Java-Program/Fibonacci.java
+++ Day3-Scuffed-Fibonacci-Java-Program/Fibonacci.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Fibonacci{
@@ -9,38 +10,39 @@ public class Fibonacci{
 
     int nthFibonacci;
 
-    System.out.print("Enter the nth fibonacci: ");
-    nthFibonacci = input.nextInt();
-    
-    int counter = 0;
-    BigInteger num1 = BigInteger.ZERO;
-    BigInteger num2 = BigInteger.ONE;
-    
-    while(true){
-      if(counter == nthFibonacci){
-        break;
-      }
-      
-      if(counter == 0){
-        System.out.println(counter + ": " + num1);
-      }
-      if(counter == 1){
-        System.out.println(counter + ": " + num2);
-        
-      }
-      if(counter >= 2){
-        BigInteger next = num1.add(num2);
-        num1 = num2;
-        num2 = next;
-        System.out.println(counter + ": " + next);
-      }
-      
-      counter++;
+    try{
+      System.out.print("Enter the nth fibonacci: ");
+      nthFibonacci = input.nextInt();
 
-
+      int counter = 0;
+      BigInteger num1 = BigInteger.ZERO;
+      BigInteger num2 = BigInteger.ONE;
+      
+      while(true){
+        if(counter == nthFibonacci){
+          break;
+        }
+        if(counter == 0){
+          System.out.println(counter + ": " + num1);
+        }
+        if(counter == 1){
+          System.out.println(counter + ": " + num2);
+        }
+        if(counter >= 2){
+          BigInteger next = num1.add(num2);
+          num1 = num2;
+          num2 = next;
+          System.out.println(counter + ": " + next);
+        }
+        counter++;
+      }
+    }catch(InputMismatchException e){
+      input.nextLine();
+      System.out.println("\nError: " + e + "\n");
+      main(args); //remove args if there's an error.
+    }finally{
+      input.close();
     }
-
-    input.close();
   }
 
 }
