84d72ec2fc61f2a86b7009dc3992e7f2bdf2bc38
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 21:18:29 +0800

    Add functionality

--- Day3-Scuffed-Fibonacci-Java-Program/Fibonacci.java
+++ Day3-Scuffed-Fibonacci-Java-Program/Fibonacci.java
@@ -1,10 +1,46 @@
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Fibonacci{
 
   public static void main(String[] args){
 
-    
+    Scanner input = new Scanner(System.in);
 
+    int nthFibonacci;
+
+    System.out.print("Enter the nth fibonacci: ");
+    nthFibonacci = input.nextInt();
+    
+    int counter = 0;
+    BigInteger num1 = BigInteger.ZERO;
+    BigInteger num2 = BigInteger.ONE;
+    
+    while(true){
+      if(counter == nthFibonacci){
+        break;
+      }
+      
+      if(counter == 0){
+        System.out.println(counter + ": " + num1);
+      }
+      if(counter == 1){
+        System.out.println(counter + ": " + num2);
+        
+      }
+      if(counter >= 2){
+        BigInteger next = num1.add(num2);
+        num1 = num2;
+        num2 = next;
+        System.out.println(counter + ": " + next);
+      }
+      
+      counter++;
+
+
+    }
+
+    input.close();
   }
 
 }
