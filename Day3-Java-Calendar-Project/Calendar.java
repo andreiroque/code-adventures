c49bd643784448a8273cf4e4b7f8f79ce90d893b
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-12 13:41:39 +0800

    Add a calendar-like design/style to the output

--- Day3-Java-Calendar-Project/Calendar.java
+++ Day3-Java-Calendar-Project/Calendar.java
@@ -17,8 +17,29 @@ public class Calendar{
       for(int month = 1; month <= 12; month++){
         LocalDate date = LocalDate.of(yearInput, month, 1);
         System.out.println(date.getMonth());
+        System.out.println("Sun\tMon\tTue\tWed\tThu\tFri\tSat");
+
+
+        // Gets the start of the week
+        // (date.getDayOfWeek().getValue() will return
+        // an int from 0-Monday ... 7-Sunday)
+        int startOfWeek = date.getDayOfWeek().getValue();
+        // Calculating what day is the start of the week.
+        startOfWeek %= 7;
+
+        // Will print spaces in the start of the month
+        // to have a calendar-like design/style output
+        for(int spaces = 0; spaces < startOfWeek; spaces++){
+          System.out.print("\t");
+        }
+
         for(int day = 1; day <= date.lengthOfMonth(); day++){
-          System.out.print(day + " ");
+          System.out.print(day + "\t");
+          // Check to see if the day reaches sat
+          // and pushes the next day to the next line.
+          if((day + startOfWeek) % 7 == 0){
+            System.out.println();
+          }
         }
         System.out.println();
         System.out.println();
