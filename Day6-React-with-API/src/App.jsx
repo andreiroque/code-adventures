442a853af8886fbb31fb9bd41346c526f34f8ce0
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-15 23:21:11 +0800

    Add basic mobile responsiveness

--- Day6-React-with-API/src/App.jsx
+++ Day6-React-with-API/src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
     try {
       const response = await fetch(url);
       const responseData = await response.json();
-      console.log(responseData.results);
       setData(responseData.results);
     } catch (error) {
       console.error("Error: ", error);
@@ -23,7 +22,7 @@ const App = () => {
     <div className="h-screen flex items-center justify-center bg-zinc-900">
       <div className="h-4/5 w-4/5 bg-zinc-400 rounded-lg p-10 overflow-auto scrollbar scrollbar-thumb-slate-600 scrollbar-track-transparent">
         <h1 className="text-2xl font-mono pb-10">Rick and Morty Characters</h1>
-        <div className="grid grid-cols-3 gap-x-10 gap-y-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-14">
           {data.map((character, index) => (
             <div className="h-60 w-auto bg-white rounded-lg p-5" key={index}>
               <div className="flex gap-10">
