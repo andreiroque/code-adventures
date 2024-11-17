ed09134d825476868a5456c03bde9db72e2a7f79
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-17 12:43:15 +0800

    Add animation using framer-motion

--- Day8-React-App-With-Framer-Motion/src/App.jsx
+++ Day8-React-App-With-Framer-Motion/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import "./App.css";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
-      <h1>Hehe</h1>
+    <div className="h-screen flex items-center justify-center flex-col bg-zinc-900">
+      <AnimatePresence>
+        {show ? (
+          <motion.div
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -100, opacity: 0 }}
+            className="bg-slate-800 p-20 rounded-lg font-mono text-gray-300"
+          >
+            <h1 className="text-3xl">Hello World!</h1>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+      <button
+        type="button"
+        className="px-20 py-10 bg-green-600 rounded-full text-gray-300 btn text-xl"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {show ? "Hide" : "Show"}
+      </button>
     </div>
   );
 };
