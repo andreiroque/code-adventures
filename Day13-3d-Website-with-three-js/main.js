0a9fc25072695493b2877c005cc2624a3ff0d415
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-22 16:43:37 +0800

    Add boilerplate

---
+++ Day13-3d-Website-with-three-js/main.js
@@ -0,0 +1,6 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+gsap.registerPlugin(ScrollTrigger);
+
+console.log(gsap);
