c9100346ff2a1d5cb789ad6ee25b8b74023f4f22
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-22 21:21:15 +0800

    Add functionality

--- Day13-Java-Cool-Hello-World-Animation/Main.java
+++ Day13-Java-Cool-Hello-World-Animation/Main.java
@@ -3,7 +3,27 @@ public class Main{
 
   public static void main(String[] args){
 
-    
+    String text = "hello world";
+
+    char[] charLookup = {'x', '#', 'a', 'p', 'q', 'w', 'd','s', 'e', 'h', 'l', '!', 'A', '$', 'v', 'o', 'm', ' ', 'g', 'E', 'j', 't', 'r', 'U', 'o', 'f', '@', 'O', ';', '|', '<'};
+
+    int counter = 0;
+    String corretText = "";
+    while(counter < text.length() - 1){
+      for(int i = 0; i < charLookup.length; i++){
+        if (charLookup[i] == text.charAt(counter)){
+          corretText += charLookup[i];
+          counter++;
+        }
+        if(corretText.length() == 0){
+          System.out.println(charLookup[i]);
+        }else if (corretText.length() > 0){
+          System.out.println(corretText + charLookup[i]);
+        }
+      }
+    }
+
+
 
   }
 
