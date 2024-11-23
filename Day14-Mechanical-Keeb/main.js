1258b15c61df1c15c8f616d26f5a2b8ac9773c75
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-23 20:58:58 +0800

    Initialize camera and scene

--- Day14-Mechanical-Keeb/main.js
+++ Day14-Mechanical-Keeb/main.js
@@ -2,3 +2,12 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const camera = new THREE.PerspectiveCamera(
+  10,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const scene = new THREE.Scene();
