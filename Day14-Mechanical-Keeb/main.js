47a00c2838af55ecc3e4868802edd25776eb4ffe
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-23 21:32:58 +0800

    Load 3d model and add lightings and Orbit Controls

--- Day14-Mechanical-Keeb/main.js
+++ Day14-Mechanical-Keeb/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,8 +11,39 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const scene = new THREE.Scene();
+camera.position.z = 13;
 
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+let model;
+const loader = new GLTFLoader();
+loader.load(
+  "./mechanical-keyboard.glb",
+  (gltf) => {
+    model = gltf.scene;
+    model.position.y -= 0.5;
+    scene.add(model);
+  },
+  (xhr) => {},
+  (err) => {}
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight, ambientLight);
+
+const control = new OrbitControls(camera, renderer.domElement);
+
+console.log(control);
+
+const reRender3D = () => {
+  requestAnimationFrame(reRender3D);
+  control.update();
+  renderer.render(scene, camera);
+};
+
+reRender3D();
