6f5add1a3a066bf3f3fe0c3715ea6942e2ab88cf
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-15 11:39:00 +0800

    Import table from shadcn

--- Day16-Simple-To-do-list/src/App.jsx
+++ Day16-Simple-To-do-list/src/App.jsx
@@ -1,12 +1,45 @@
+import { useRef, useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const App = () => {
+  const inputRef = useRef();
+  const [tasks, setTask] = useState([]);
+
+  const addTask = () => {
+    console.log(inputRef.current.value);
+    inputRef.current.value = "";
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center flex-col">
       <div className="flex gap-5">
-        <Input placeholder="Task" />
-        <Button variant="outline">Add task</Button>
+        <Input ref={inputRef} placeholder="Task" />
+        <Button variant="outline" onClick={addTask}>
+          Add task
+        </Button>
+      </div>
+      <div className="mt-5">
+        <Table>
+          <TableCaption>To-do List</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Task</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody></TableBody>
+        </Table>
       </div>
     </div>
   );
