30292ed8d1bcb90979b8317952e8d8e45ce4f051
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-18 20:53:55 +0800

    Add button component

--- Day18-React-components/src/App.jsx
+++ Day18-React-components/src/App.jsx
@@ -1,10 +1,12 @@
+import Button from "./components/Button";
 import Input from "./components/Input";
 
 const App = () => {
   return (
     <div className="h-screen flex items-center justify-center gap-5">
-      <div>
+      <div className="flex gap-5">
         <Input text="Task" />
+        <Button text="Add task" />
       </div>
     </div>
   );
---
+++ Day18-React-components/src/components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({ text }) => {
+  return (
+    <button className="bg-blue-600 text-white px-5 rounded-md">{text}</button>
+  );
+};
+
+export default Button;
