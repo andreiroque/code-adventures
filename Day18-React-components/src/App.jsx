fc6317ae75e24c6fa5727198fcbc730606206f25
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-18 20:39:23 +0800

    Fix code

--- Day18-React-components/src/App.jsx
+++ Day18-React-components/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div className="h-screen flex items-center justify-center flex-col gap-5 font-mono">
-      <h1 className="lg:text-8xl">Hello, World!</h1>
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
 };
